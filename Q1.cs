079004d80126ab6082bade4bde5fab0479d8042c
Sia Nadkarni <149247333+SiaNadkarni@users.noreply.github.com>
2023-10-28 20:06:44 +0530

    Add files via upload

---
+++ Q1.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static string Main(string[] args)
+    {
+        int i,j,n;
+        string  "a[]" ;
+        
+
+Console.Write("Enter the array of data:");
+    
+        a[]= Console.ReadLine();
+        n=a.Length; 
+        
+
+        for (i =0;i<=n ;i++)
+        {
+            if (!(a[i] = a[i + 1]))
+            { Console.WriteLine("False"); }
+            else
+            {
+                Console.WriteLine("True");
+
+            }
+
+
+
+        }
+        for(i= 0;i<=n ;i++)
+        { }
+        
+
+
+
+        Console.ReadLine(); 
+    }
+}
---
+++ Q7.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] Arr = 21, 43, 22, 67 ,43, 12 ,4;
+        int i, j;
+
+        for (i = 0; i <= 7; i++)
+        {
+            for (j = 7; j <= 7; j--)
+            {
+                Arr[i] = Arr[j];
+            }
+            Console.Write(Arr[i]);
+        }
+    }
+}
